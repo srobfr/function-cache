66b53f11e09726c3733ea1b2b75f5a78a201a83f
Simon Robert <srob@srob.fr>
2017-01-02 14:48:32 +0100

    Ajout d'une méthode flush()

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ function cache(func, options) {
     // On calcule un hash pour la fonction, de manière à invalider le code si elle change.
     var funcHash = options.hasher(func.toString());
 
-    var funcWrapper = function() {
+    var funcWrapper = function () {
         if (!options.useMemoryCache && !options.useFileCache) return func;
 
         var args = _.toArray(arguments);
         var serializedArgs = options.serializer(args);
-        var hash = options.hasher(funcHash + serializedArgs);
+        var hash = "" + funcHash + options.hasher(serializedArgs);
         var p = (options.useFileCache ? options.tmpDir + path.sep + options.tmpPrefix + hash : null);
 
         if (options.useMemoryCache && storage[hash] !== undefined) {
@@ -40,15 +40,15 @@ function cache(func, options) {
 
         function callFunc() {
             return Q(func.apply(this, args))
-                .then(function(result) {
+                .then(function (result) {
                     if (options.useMemoryCache) storage[hash] = result;
                     if (options.useFileCache) {
                         var dir = path.dirname(p);
                         // On crée le dossier s'il n'existe pas.
                         mkdirParents(dir, 0777, function (err) {
                             if (err) throw new Error(err);
-                            fs.writeFile(p, options.serializer(result), function(err) {
-                                if(err) throw new Error(err);
+                            fs.writeFile(p, options.serializer(result), function (err) {
+                                if (err) throw new Error(err);
                             });
                         });
                     }
@@ -59,23 +59,46 @@ function cache(func, options) {
 
         if (options.useFileCache) {
             return Q()
-                .then(function() {
+                .then(function () {
                     // On vérifie si un cache existe sous forme de fichier.
                     return Q.nfcall(fs.readFile, p, "UTF-8");
                 })
-                .then(function(serializedData) {
-                    // Le cache est disponible
-                    var data = options.unserializer(serializedData);
-                    if(options.useMemoryCache) storage[hash] = data;
-                    return data;
-                },
-                function(err) {
-                    // Cache indisponible.
-                    return callFunc();
-                });
+                .then(function (serializedData) {
+                        // Le cache est disponible
+                        var data = options.unserializer(serializedData);
+                        if (options.useMemoryCache) storage[hash] = data;
+                        return data;
+                    },
+                    function (err) {
+                        // Cache indisponible.
+                        return callFunc();
+                    });
         }
 
         return callFunc();
+    };
+
+    funcWrapper.flush = function () {
+        if (options.useMemoryCache) {
+            var hashes = _.keys(storage);
+            _.each(hashes, function (hash) {
+                if (hash.indexOf(funcHash) === 0) delete storage[hash];
+            });
+        }
+
+        if (!options.useFileCache) Q();
+
+        var pDirContent = Q.nfcall(fs.readdir, options.tmpDir, "UTF-8");
+
+        return pDirContent.then(function (dirContent) {
+            var filteredDirContent = _.filter(dirContent, function (p) {
+                return (p.indexOf(options.tmpPrefix + funcHash) === 0);
+            });
+
+            return Q.all(_.map(filteredDirContent, function (p) {
+                return Q.nfcall(fs.unlink, options.tmpDir + path.sep + p);
+            }));
+        });
     };
 
     return funcWrapper;
